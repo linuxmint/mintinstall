52adfd52c5dab424bb514633facaee36e01f01a7
darealshinji <djcj@gmx.de>
2014-05-21 18:40:54 +0200

    update

--- usr/lib/linuxmint/mintInstall/mintInstall.py
+++ usr/lib/linuxmint/mintInstall/mintInstall.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # mintInstall
-#	No Copyright (What for?) Clem <root@linuxmint.com>
+#	Copyright Clem <root@linuxmint.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
