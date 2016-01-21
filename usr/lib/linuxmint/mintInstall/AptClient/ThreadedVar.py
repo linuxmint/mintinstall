acbcacb9366c7728a352d1cbe1aeaa9778c6d6df
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2016-01-21 16:20:11 +0000

    Shebangs

--- usr/lib/linuxmint/mintInstall/AptClient/ThreadedVar.py
+++ usr/lib/linuxmint/mintInstall/AptClient/ThreadedVar.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python
-# -*- coding=utf-8 -*-
 
 import threading
 
@@ -7,13 +5,13 @@ class ThreadedVar(object):
     def __init__(self, value = None):
         self._value = value
         self._lock = threading.Lock()
-    
+
     def get_value(self):
         self._lock.acquire()
         res = self._value
         self._lock.release()
         return res
-    
+
     def set_value(self, value):
         self._lock.acquire()
         self._value = value
--- usr/lib/linuxmint/mintInstall/AptClient/test.py
+++ usr/lib/linuxmint/mintInstall/AptClient/test.py
@@ -1,5 +1,4 @@
-#! /usr/bin/python
-# test.py
+#! /usr/bin/python2
 # -*- Mode: Python; indent-tabs-mode: nil; tab-width: 4; coding: utf-8 -*-
 
 from AptClient import AptClient
@@ -11,20 +10,20 @@ class TestApp(object):
         self._apt_client.connect("idle", lambda c: gtk.main_quit())
         self._apt_client.connect("task_ended", self._on_task_ended)
         self._apt_client.connect("progress", self._on_progress)
-    
+
     def _on_progress(self, apt_client, progress, status):
         print "_on_progress:", progress, status
-    
+
     def _on_task_ended(self, apt_client, task_id, task_type, task_params, success, error):
         print "\t\t_on_task_ended : ", apt_client, task_id, task_type, task_params, success, error
-    
+
     def _start_tasks(self):
         #self._apt_client.update_cache()
         self._apt_client.install_package("phpmyadmin")
         #self._apt_client.install_package("gedit")
         #self._apt_client.install_package("gnome-xcf-thumbnailer")
         return False
-    
+
     def run(self):
         gobject.threads_init()
         gobject.timeout_add(100, self._start_tasks)
@@ -32,5 +31,4 @@ class TestApp(object):
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.FATAL)
-    
     TestApp().run()
