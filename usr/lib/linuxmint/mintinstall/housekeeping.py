3be3a5d2d971be9a2535f63ae47fdf3549029ed7
Michael Webster <miketwebster@gmail.com>
2018-04-22 18:32:28 -0400

    Add script to periodically clean out old screenshots.

---
+++ usr/lib/linuxmint/mintinstall/housekeeping.py
@@ -0,0 +1,43 @@
+import sys
+if sys.version_info.major < 3:
+    raise "python3 required"
+
+import gi
+from gi.repository import GLib
+
+import time
+import os
+import threading
+import multiprocessing
+from pathlib import Path
+
+SCREENSHOT_DIR = os.path.join(GLib.get_user_cache_dir(), "mintinstall", "screenshots")
+
+MAX_AGE = 14 * (60 * 60 * 24) # days
+
+def run():
+    print("MintInstall: deleting old screenshots")
+
+    thread = threading.Thread(target=_clean_screenshots_thread)
+    thread.start()
+
+def _clean_screenshots_thread():
+    proc = multiprocessing.Process(target=_clean_screenshots_process)
+
+    proc.start()
+    proc.join()
+
+def _clean_screenshots_process():
+    ss_location = Path(SCREENSHOT_DIR)
+
+    screenshots = ss_location.glob("*.*")
+
+    for p in screenshots:
+        try:
+            mtime = os.path.getmtime(p)
+
+            if (time.time() - MAX_AGE) > mtime:
+                p.unlink()
+        except OSError:
+            pass
+
